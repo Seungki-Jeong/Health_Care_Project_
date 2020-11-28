d4b761bb4e2e6fb75d09b21f8785df0cc2d13921
seungki <fex5360@gmail.com>
2020-11-28 23:02:06 +0900

    health_care

---
+++ README.md
@@ -0,0 +1,8 @@
+# Health_Care_project
+## 2020-09-19
+- bootstrap4 -> node.js code 작성
+- firebase 작성
+
+## 2020-09-20
+- index.js 간소화
+- navi menu 간소화
---
+++ health_care_express/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var url = require('url');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var pathname = url.parse(req.url).pathname;
+  res.render('index', { title: 'Health Care - Main', pathnames: pathname});
+});
+
+router.get('/squat', function (req, res, tnext) {
+  var pathname = url.parse(req.url).pathname;
+  res.render('index', {title: 'Health Care - squart', name:'squat', pathnames: pathname});
+});
+
+router.get('/contact', function (req, res) {
+  var pathname = url.parse(req.url).pathname;
+  res.render('index', {title: 'Health Care - contact', pathnames: pathname});
+
+})
+
+
+module.exports = router;
